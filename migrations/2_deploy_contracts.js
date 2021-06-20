fdc23a0304146b9bc9b62aaf3d0c13206abdab6e
anupamakoirala <anukoirala.96@hotmail.com>
2021-06-20 10:58:56 +0545

    Contract added

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var Crowdfund = artifacts.require("./Crowdfund.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(Crowdfund);
 };
